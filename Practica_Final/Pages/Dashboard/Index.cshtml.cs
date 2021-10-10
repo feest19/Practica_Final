632c58ed378fc6d9bab53a22a5353d8d0067af9f
Trombo14 <50005334+Trombo14@users.noreply.github.com>
2021-10-09 23:18:56 -0400

    Agregando NumCuentas del usuario al dashboard.  Agregando Tabla con los Ultimos 5 transacciones hecha al usuario.

--- Practica_Final/Pages/Dashboard/Index.cshtml.cs
+++ Practica_Final/Pages/Dashboard/Index.cshtml.cs
@@ -6,16 +6,43 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Security.Claims;
+using Practica_Final.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Practica_Final.Pages.Dashboard
 {
     [Authorize(AuthenticationSchemes = "Cookies")]
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly Infrastructure.Contexts.ApplicationDbContext _context;
 
-            string idUsuario = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+        public IndexModel(Infrastructure.Contexts.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public List<CuentaBancaria> Cuentas { get; set; } 
+        public List<Transferencia> transferencias { get; set; }
+        public string Usuario { get; set; }
+        //public void OnGet()
+        //{
+        //    string idUsuario = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+           
+        //}
+        public async Task OnGetAsync()
+        {
+            int idUsuario;
+            bool success = Int32.TryParse(this.User.FindFirstValue(ClaimTypes.NameIdentifier), out idUsuario);
+
+            if (success)
+            {
+                var historyTransf = from c in _context.CuentasBancarias
+                                  join t in _context.Transferencias on c.Id equals t.CuentaBancariaDestinatarioId 
+                              where c.UsuarioId == idUsuario
+                              orderby t.Fecha descending
+                              select t;
+              this.Cuentas = await _context.CuentasBancarias.Where(x => x.UsuarioId == idUsuario).ToListAsync();
+                this.transferencias = await historyTransf.Take(5).ToListAsync();
+            }
         }
     }
 }
